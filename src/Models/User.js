1d767dcced40ee326dd3b167fac26cac1877d52c
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2017-12-15 09:56:05 +0700

    - User model schema

---
+++ src/Models/User.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+/**
+ * Define user schema
+ * @type {mongoose.Schema}
+ */
+const user = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+export default mongoose.model('User', user);
