64608eb6275579b093c90cd4312a72c37ab3bddb
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2017-12-18 15:30:06 +0700

    - reset cached git

---
+++ src/Responses/NotFoundResponse.js
@@ -0,0 +1,9 @@
+export default (res, message, code = 0) => {
+    res.status(404);
+    res.json({
+        success: false,
+        error_code: code,
+        message: message,
+        data: null
+    });
+}
